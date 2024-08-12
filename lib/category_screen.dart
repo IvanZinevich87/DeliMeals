809f8bf5e4a0b66e03f45229fd4d047d33a76f1d
vazn <zinevichivan0@gmail.com>
2024-08-12 14:51:57 +0300

    styling

--- lib/category_screen.dart
+++ lib/category_screen.dart
@@ -4,15 +4,17 @@ import './dummy_data.dart';
 import 'category_item.dart';
 
 class CategoryScreen extends StatelessWidget {
-  const CategoryScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("DeliMeal"),
+        backgroundColor: Theme.of(context).primaryColor,
+        title: Text(
+          "DeliMeal",
+        ),
       ),
       body: GridView(
+        padding: EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
             .map(
               (catData) => CategoryItem(
@@ -22,7 +24,7 @@ class CategoryScreen extends StatelessWidget {
             )
             .toList(),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
+          maxCrossAxisExtent: MediaQuery.of(context).size.height * 0.3,
           mainAxisSpacing: 20,
           crossAxisSpacing: 20,
           childAspectRatio: 3 / 2,
